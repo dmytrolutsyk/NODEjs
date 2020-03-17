4a491192050eaee8e57d68558fa6d0321298b8d5
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2020-03-17 10:32:54 +0100

    maj exo 3

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 app.get('/hello', function (req, res) {
-    if(req.query != undefined) {
-        res.send("Bonjour, " + req.query.nom+"!")
+    const name = req.query.nom
+    if(name) {
+        res.send("Bonjour, " + name+"!")
     }else {
         res.send("Quel est votre nom ?")
     }
 })
 app.listen(PORT, function () {   
-    console.log('Example app listening on port ${PORT}!')
+    console.log('Example app listening on port'+PORT)
 })
