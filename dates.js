d4b5d61f4852a308c7839dda688eb83a681508d2
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2020-03-19 12:05:00 +0100

    partie 2 exo 2

--- dates.js
+++ dates.js
@@ -1,11 +1,11 @@
-
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:admin@cluster0-sluc0.azure.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(async err => {
-    const collection = client.db("test").collection("dates");
+    const collection = client.db("chat-box").collection("messages");
     // affiche la liste des documents de la collection dates dans la sortie standard
     const dates = await collection.find({}).toArray();
     console.log('dates:', dates)
-    client.close();
+    await collection.insertOne({ date: new Date() });
+    await client.close();
   });
